9131937750557226abc93ba49b152ad1bdfc0c35
Deivid Almeida <mr.davialmeida@hotmail.com>
2022-12-04 22:05:11 -0300

    :sparkles: gerate publisher

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use chrono::Utc;
 use tokio;
+use bson::doc;
 mod services;
 
 #[tokio::main]
 async fn main() {
-  services::read().await.unwrap();
+  let who: u8 =0;
+  if who == 0 {
+    services::read().await.unwrap();
+  } else {
+    services::create_publisher(doc! {
+      "name":"Madalena",
+      "type":3,
+      "amount":0,
+      "active":true,
+      "created_at": Utc::now(),
+      "updated_at": Utc::now(),
+    }).await.unwrap();
+  }
 }
-
-
-
-
 //https://github.com/mehmetsefabalik/rust-mongodb-example
 //https://doc.rust-lang.org/book/ch05-03-method-syntax.html
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,16 +1,35 @@
-use futures::TryStreamExt;
-use mongodb::{bson::Document, Client, Collection, Cursor};
-use std::error::Error;
+use bson::doc;
+use futures::{ TryStreamExt};
+use mongodb::{bson::Document, Client, Collection, Cursor, options::FindOptions};
+use std::{error::Error, time::Duration};
+use rand::Rng;
 
 mod connection; 
 
 pub async fn read()-> Result<(), Box<dyn Error>>  {
-  let client:Client =  connection::mongodb().await.unwrap();
-  let coll:Collection<Document> =  client.database("local").collection::<Document>("messages");
-  let mut cursor:Cursor<Document> = coll.find(None, None).await?;
+  let client:Client =  connection::mongodb().await?;
+  let options: FindOptions = FindOptions::builder()
+  .limit(3)
+  .sort(doc! { "amount": 1 })
+  .build();
+
+  let coll:Collection<Document> =  client.database("designations").collection::<Document>("publishers");
+  let cursor:Cursor<Document> = coll.find(doc! {"type":{"$gt":1}}, options).await?;
  
-  while let  Some(doc) = cursor.try_next().await?  {
-    println!("{}", doc);
-  }
+  let mut rng = rand::thread_rng();
+
+  let results: Vec<Document> = cursor.try_collect().await.unwrap();
+  println!("{:?}", results[rng.gen_range(0..3)]);
   Ok(())
 }
+
+pub async fn create_publisher(designation: Document)-> Result<(), Box<dyn Error>>  {
+  let client:Client =  connection::mongodb().await?;
+  let collection:Collection<Document> = client.database("designations").collection("publishers");
+  let handle=  tokio::task::spawn(async move {
+    collection.insert_one(designation, None).await
+  });
+  tokio::time::timeout(Duration::from_secs(5), handle).await???;
+  Ok(())
+}
+ 
